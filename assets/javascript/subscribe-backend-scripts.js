0e3e3d592e6f73530d30534b335255f8cb736573
Jorge Andrade <j.andrade.dev@gmail.com>
2015-03-12 10:17:11 -0600

    added the export of code

--- assets/javascript/subscribe-backend-scripts.js
+++ assets/javascript/subscribe-backend-scripts.js
@@ -2,7 +2,6 @@ $(document).on('ready', function () {
 	var $table = $('table');
 	var $btn_export = $('#btn-export');
 	$btn_export.on('click', function () {
-		alert('j');
 		$table.tableExport({type:'excel',escape:'false'});
 	});
 });
